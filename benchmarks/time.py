d662764a991d4fe3e320ed38e18ae41862da4970
Albert Y. Shih <ayshih@gmail.com>
2022-08-29 23:23:52 -0400

    Cleans up existing ASV tests and adds new ones

---
+++ benchmarks/time.py
@@ -0,0 +1,22 @@
+from sunpy.time import is_time, parse_time
+
+
+# Testing sunpy.time.is_time()
+
+def time_is_time(self):
+    is_time('1995-12-31 23:59:60')
+
+
+# Testing sunpy.time.parse_time()
+
+def time_parse_time(self):
+    parse_time('1995-12-31 23:59:60')
+
+
+def mem_parse_time(self):
+    t = parse_time('1995-12-31 23:59:60')
+    return t
+
+
+def peakmem_parse_time(self):
+    t = parse_time('1995-12-31 23:59:60')
